d8e49d51b5bb419c2d3cc5c3441efa492b69a25c
Liam Banga <liam205@outlook.com>
2020-08-02 18:24:08 +1000

    Update the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,34 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import style from "./header.module.css"
+import { Link } from "gatsby"
+import mail from '../images/mail_icon.svg'
+import github from '../images/github_icon.svg'
+import linkedin from '../images/linkedin_icon.svg'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div className={style.socials}>
+    <img src={mail} />
+    <img src={linkedin}/>
+    <img src={github} />
+    </div>
+    <div className={style.siteNav}>
+    <Link className={style.siteLinks} to="#">Home</Link>
+    <Link className={style.siteLinks} to="#">Experience</Link>
+    <Link className={style.siteLinks} to="#">Projects</Link>
+    <Link className={style.siteLinks} to="#">Get in touch</Link>
+    <Link className={style.siteLinks} to="#">Resume</Link>
     </div>
   </header>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+//Icons for: Email, Linkedin , Github
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+//Home
+//Experience
+//Projects
+//Get in Touch
+//Resume
 
 export default Header

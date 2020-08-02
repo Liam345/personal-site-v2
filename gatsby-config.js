fd964c716fd85292196743108718785ca1c38c18
Liam Banga <liam205@outlook.com>
2020-08-02 20:10:38 +1000

    Initial header with Fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,22 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    }, 
+    {
+    resolve: `gatsby-plugin-prefetch-google-fonts`,
+    options: {
+    fonts: [{
+            family: `Open Sans`,
+            variants: [`300`, `400`,`500`, `600`]
+        }, {
+          family: `Montserrat`,
+          variants: [`400`,`500`]
+        }, {
+          family: `Comfortaa`,
+          variants: [`300`,`400`,`500`,`600`]
+        }],
     },
+  }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
